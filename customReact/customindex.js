a9f18590572a337d03219a369e3ca867a28634c9
Bhavesh parmar <bhaveshparmar01012000@gmail.com>
2026-02-19 23:10:21 +0530

    that's the js for rendering

---
+++ customReact/customindex.js
@@ -0,0 +1,36 @@
+function customRender(reactElement, container) {
+    /*
+        //create DOM element
+        const domElement = document.createElement(reactElement.type)//here it's empty container
+        domElement.innerHTML = reactElement.children //add children
+        domElement.setAttribute('href', reactElement.props.href) //set elements
+        domElement.setAttribute('target', reactElement.props.target)
+        container.appendChild(domElement)//
+        */
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const props in reactElement.props) {
+        if (props === 'children') continue;
+        domElement.setAttribute(props, reactElement.props[props])
+
+    }
+    container.appendChild(domElement)
+
+}
+
+const reactElement = {
+    type: 'a',
+    props: {//it's object 
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const mainContainer = document.querySelector('#root')
+
+customRender(reactElement, mainContainer)
+
+
+
