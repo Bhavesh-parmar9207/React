0a31ed97bf54d18ed615577f36232ba020f7a81c
Bhavesh parmar <bhaveshparmar01012000@gmail.com>
2026-02-26 00:06:36 +0530

    bg changer project created

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,21 +3,27 @@ import { useState } from 'react'
 
 function App() {
 
-  let [counter, setcounter] = useState(0)
-  const hanleAdd = () => {
-    setcounter(counter + 1)
+  let [counter, setcounter] = useState(15)
+  //usestate send the opration command in batches to the variables | if we give same command for multiple times like counter + 1 then it will execute only once
+  //so we use call back function to update the value
+
+  //set counter is function ehic also takes call back function as an argument
+
+  //for taking that we need to take prevcounter as an argument in setcounter because with that method it callback every times with new values and give added values 
+  const addValue = () => {
+    setcounter(prevCounter => prevCounter + 1)
   }
-  const handleRemove = () => {
-    setcounter(counter - 1)
+  const removeValue = () => {
+    setcounter(prevCounter => prevCounter - 1)
   }
 
   return (
     <>
       <h1>Hello World!</h1>
-      <h2>Count value : {counter}</h2> 
+      <h2>Count value : {counter}</h2>
 
-      <button onClick={hanleAdd}>Add button</button>  <br/>
-      <button onClick={handleRemove}>Remove button</button>  
+      <button onClick={addValue}>Add button{counter}</button>  <br />
+      <button onClick={removeValue}>Remove button{counter}</button>
     </>
   )
 }
